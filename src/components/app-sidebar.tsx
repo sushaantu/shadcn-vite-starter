4662bb0b83933ac461ed32a1da212341a9e41d22
Sushaantu <github@sushaantu.com>
2025-05-13 23:49:28 -0400

    Refactor App and AppSidebar components to improve structure and navigation. Rename Layout to Page, introduce user data, and replace static sidebar items with dynamic NavMain and NavSecondary components.

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,82 +1,123 @@
-import { Building, ChartArea, LayoutDashboard, LifeBuoy, Receipt, ScanSearch, Settings, Users } from "lucide-react"
+"use client"
 
+import * as React from "react"
+import {
+  Command,
+  LayoutDashboard,
+  LifeBuoy,
+  Receipt,
+  ScanSearch,
+  Send,
+  Settings,
+  ChartArea,
+  Building,
+  Users,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavSecondary } from "@/components/nav-secondary"
+import { NavUser } from "@/components/nav-user"
 import {
   Sidebar,
   SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
+  SidebarFooter,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 
-// Menu items.
-const items = [
-  {
-    title: "Dashboard",
-    url: "#",
-    icon: LayoutDashboard,
+const data = {
+  user: {
+    name: "Sushaantu",
+    email: "sushaantu@joindeleteme.com",
+    avatar: "/avatars/shadcn.jpg",
   },
-  {
-    title: "Members",
-    url: "#",
-    icon: Users,
-  },
-  {
-    title: "Reports",
-    url: "#",
-    icon: ChartArea,
-  },
-  {
-    title: "Organizations",
-    url: "#",
-    icon: Building,
-  },
-  {
-    title: "Risk Scan",
-    url: "#",
-    icon: ScanSearch,
-  },
-  {
-    title: "Billing",
-    url: "#",
-    icon: Receipt,
-  },
-  {
-    title: "Setting",
-    url: "#",
-    icon: Settings,
-  },
-  {
-    title: "Support",
-    url: "#",
-    icon: LifeBuoy,
-  },
-]
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: LayoutDashboard,
+      isActive: true
+    },
+    {
+      title: "Members",
+      url: "#",
+      icon: Users,
+      isActive: true
+    },
+    {
+      title: "Reports",
+      url: "#",
+      icon: ChartArea,
+      isActive: true
+    },
+    {
+      title: "Organizations",
+      url: "#",
+      icon: Building,
+      isActive: true
+    },
+    {
+      title: "Risk Scan",
+      url: "#",
+      icon: ScanSearch,
+      isActive: true
+    },
+    {
+      title: "Billing",
+      url: "#",
+      icon: Receipt,
+      isActive: true
+    },
+    {
+      title: "Setting",
+      url: "#",
+      icon: Settings,
+      isActive: true
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Support",
+      url: "#",
+      icon: LifeBuoy,
+    },
+    {
+      title: "Feedback",
+      url: "#",
+      icon: Send,
+    },
+  ],
+}
 
-export function AppSidebar() {
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar>
+    <Sidebar variant="inset" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                  <Command className="size-4" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">Acme Inc</span>
+                  <span className="truncate text-xs">Enterprise</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
     </Sidebar>
   )
 }
